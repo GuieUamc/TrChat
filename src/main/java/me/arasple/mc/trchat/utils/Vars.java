af5ce0cd1ac30aaa686ff46f002b39ddc19da3d8
HikariLan <shaokeyibb@126.com>
2021-02-08 11:01:31 +0800

    Version 1.72
    This version compatible with the latest version of PlaceHolderAPI(2.10.9),which fixed the bug that the plugins crash because of the new CloudExpansionAPI

--- src/main/java/me/arasple/mc/trchat/utils/Vars.java
+++ src/main/java/me/arasple/mc/trchat/utils/Vars.java
@@ -5,6 +5,7 @@ import me.arasple.mc.trchat.TrChat;
 import me.arasple.mc.trchat.TrChatFiles;
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.PlaceholderAPIPlugin;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.clip.placeholderapi.expansion.cloud.CloudExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -30,21 +31,21 @@ public class Vars {
      * @param expansions 拓展
      */
     public static void downloadExpansions(List<String> expansions) {
-        if (PlaceholderAPIPlugin.getInstance().getExpansionCloud() == null) {
+        if (PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().getCloudExpansions() == null) {
             return;
         }
         try {
             if (expansions != null && expansions.size() > 0) {
-                if (PlaceholderAPIPlugin.getInstance().getExpansionCloud().getCloudExpansions().isEmpty()) {
-                    PlaceholderAPIPlugin.getInstance().getExpansionCloud().fetch(false);
+                if (PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().getCloudExpansions().isEmpty()) {
+                    PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().fetch(false);
                 }
-                List<String> unInstalled = expansions.stream().filter(d -> PlaceholderAPI.getExpansions().stream().noneMatch(e -> e.getName().equalsIgnoreCase(d)) && PlaceholderAPIPlugin.getInstance().getExpansionCloud().getCloudExpansion(d) != null && !PlaceholderAPIPlugin.getInstance().getExpansionCloud().isDownloading(d)).collect(Collectors.toList());
+                List<String> unInstalled = expansions.stream().filter(d -> PlaceholderAPIPlugin.getInstance().getLocalExpansionManager().getExpansions().stream().noneMatch(e -> e.getName().equalsIgnoreCase(d)) && PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().findCloudExpansionByName(d).isPresent() && !PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().isDownloading(PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().findCloudExpansionByName(d).get())).collect(Collectors.toList());
                 if (unInstalled.size() > 0) {
                     unInstalled.forEach(ex -> {
-                        CloudExpansion cloudExpansion = PlaceholderAPIPlugin.getInstance().getExpansionCloud().getCloudExpansion(ex);
-                        PlaceholderAPIPlugin.getInstance().getExpansionCloud().downloadExpansion(null, cloudExpansion);
+                        CloudExpansion cloudExpansion = PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().getCloudExpansions().get(ex);
+                        PlaceholderAPIPlugin.getInstance().getCloudExpansionManager().downloadExpansion(cloudExpansion, cloudExpansion.getVersion());
                     });
-                    Bukkit.getScheduler().runTaskLater(TrChat.getPlugin(), () -> PlaceholderAPIPlugin.getInstance().getExpansionManager().registerAllExpansions(), 20);
+                    Bukkit.getScheduler().runTaskLater(TrChat.getPlugin(), () -> PlaceholderAPIPlugin.getInstance().getLocalExpansionManager().getExpansions().forEach((it) -> PlaceholderAPIPlugin.getInstance().getLocalExpansionManager().register(it)), 20);
                 }
             }
         } catch (Throwable ignored) {
